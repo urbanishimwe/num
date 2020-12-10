8f39c9208139f972d65ddace7634e7f76108fabd
Urban Ishimwe <urbainishimwe@gmail.com>
2020-12-10 13:38:49 +0200

    validate hex that ends with "b"

--- num.go
+++ num.go
@@ -126,6 +126,9 @@ func outputInt(v string) string {
 	case endsWithFold(vToB, []byte("pb")):
 		t2 = PB
 	case endsWithFold(vToB, []byte("b")):
+		if len(vToB) > 2 && (vToB[1] == 'x' || vToB[1] == 'X') {
+			break
+		}
 		cut = 1
 	default:
 		cut = 0
